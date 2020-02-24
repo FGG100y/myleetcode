11cefccc15149025fbf60aa24c238b28d5467090
FGG100y <1522009317@qq.com>
2020-02-24 16:17:26 +0800

    longest substring with no repeat chars

---
+++ lss.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# lss common method
+
+
+def longest_ss(s):
+    # start, end to keep tracking the current substring
+    # 'end' begin at index i=0, move forword, if  a second char at index j,
+    # store the candidate lss=s[i:j], iff len(lss) < j-i+1, update the lss
+    # then, update index i to i+1, and keep searching forword
+    lookup = set()
+    i, j = 0, 0
+    while j <= len(s):
+        if j == len(s):  # due to the string slice nature
+            lookup.add(s[i:])
+            break
+        while s[j] in s[i:j]:
+            print('*', s[i:j])
+            lookup.add(s[i:j])
+            i += 1
+        j += 1
+    lss = ''
+    for s in lookup:
+        if len(lss) < len(s):
+            lss = s
+    print('='*len(lss))
+    return lss
+
+
+if __name__ == "__main__":
+    s = 'asdkffsdd,'
+    print(s)
+    print('='*len(s))
+    print(longest_ss(s))
