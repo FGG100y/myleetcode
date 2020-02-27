c50a6174d5777657631c7b1fdec939923fb1b3b9
fgg100y <1522009317@qq.com>
2020-02-27 21:17:30 +0800

    reoranize the directories/files names

---
+++ hwtt/common_lss.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# search for the longest common substring of two strings
+
+
+def common_lss(s1, s2):
+    pass
+
+
+if __name__ == "__main__":
+    s1 = 'abcdefghijklmnop'
+    s2 = 'abcsafjklmnopqrstuvw'
+    print(common_lss(s1, s2))
