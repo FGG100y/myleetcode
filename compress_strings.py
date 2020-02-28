325e03b85df1ce8e7639c95db667f46e9e277ce4
FGG100y <1522009317@qq.com>
2020-02-28 14:54:01 +0800

    hwtt compress strings of the repeated chars

---
+++ compress_strings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# 'xxxbyyyyymm' --> '3xb5y2m'
+
+
+def compress_string(s):
+    res = ''
+    i, j = 0, 0
+    while i < len(s):
+        while j <= len(s) and s[i] == s[j]:
+            j += 1
+            if j >= len(s):
+                break
+        if j - i == 1:
+            res += s[i]
+        else:
+            res += str(j - i) + s[i]
+        print(res)
+        i = j
+    return res
+
+
+if __name__ == '__main__':
+    s = 'xxbxyyyyymm'
+    print(s)
+    print('*'*len(s))
+    print(compress_string(s))
