9686eb2b7d031eb21ebdad2de275bdd18a3587b4
fgg100y <1522009317@qq.com>
2020-03-15 22:21:56 +0800

    add __repr__ method to enhance object printing
    when the instances were in list, python did not auto call the __str__ to
    print the object's name, but instead print the its memory address.
    Adding the __repr__ method to deal with this issue.

--- hwtt/optimization_graph.py
+++ hwtt/optimization_graph.py
@@ -17,6 +17,10 @@ class Node(object):
     def __str__(self):
         return self.name
 
+    # to print each element in list
+    def __repr__(self):
+        return self.__str__()
+
 
 class Edge(object):
     """description"""
