4ab39782bda8c1c280d31cbf66614655bce31d62
fgg100y <1522009317@qq.com>
2020-06-20 13:13:57 +0800

    add two more exercises
    - 24point games
    - narcissistic numbers

---
+++ hwtt/narcissistic_number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# 水仙花数(Narcissistic number, aka PPDI): 153 = 1**3 + 5**3 + 3**3
+# 完全数字不变数(Perfect digital invariant): 4150 = 4**5 + 1**5 + 5**5 + 0**5
+# 水仙花数一定是‘完全数字不变数’，反之不然；严格意义上水仙花数指三位数。
+
+
+def is_Narcissistic(s):
+    pass
